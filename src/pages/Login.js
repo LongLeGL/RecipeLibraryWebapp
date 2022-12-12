e4690f6ee95d7e482c9741656394ef1cb3954bed
Long Le <lhlong1542002@gmail.com>
2022-12-12 09:27:38 +0700

    finished login page UI

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,38 @@
 import './Login.css';
 import './global.css'
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(email, password);
+    window.location.href = '/';
+  }
+
   return (
     <div className='LoginPage'>
-      Login page
+      <div className='LoginPageTitle'>
+        <img src='/logo.png' alt='logo' />
+        <h1>Recipe Library</h1>
+      </div>
+
+      <form>
+        <div className='InputContainer'>
+          <label>Username or Email</label>
+          <input type="text" name="name" onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className='InputContainer'>
+          <label>Password</label>
+          <input type="password" name="password" onChange={e => setPassword(e.target.value)} />
+        </div>
+        <input type='submit' value='Login' onClick={handleSubmit} />
+
+      </form>
     </div>
   );
 }
