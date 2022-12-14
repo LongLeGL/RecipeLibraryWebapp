88aabca0ab9962fcc71a46ce372f8be67a0fc76c
luutrinhthien <luutrinhthien@gmail.com>
2022-12-14 08:16:10 +0700

    create and view recipe

--- src/pages/CreateRecipe.js
+++ src/pages/CreateRecipe.js
@@ -1,10 +1,77 @@
 import React from 'react';
 import './CreateRecipe.css'
+import Autocomplete from '@mui/material/Autocomplete';
+import Chip from '@mui/material/Chip';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 function CreateRecipe() {
-    return ( 
+    const Tags = ['Tatsy', 'Chicken', 'Pizza', 'Noodle', 'CleanEating', 'HealthyFood', 'JustEatRealFood', 'VeganFood', 'HealthyFoodRecipes', 'HealthyFoodLover', 'Popcorn']
+    return (
         <div className='CreateRecipe'>
-            Recipe creation page
+            <div className='main-bodypart'>
+                <div className='recipeName' style={{ display: 'flex', alignItems: 'center' }}>
+                    <h3>Recipe Name: </h3>
+                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                        <TextField style={{ width: '40vw' }} id="outlined-basic" label="Your input..." variant="outlined" />
+                    </Box>
+                </div>
+                <div style={{ paddingTop: '20px' }}>
+                    <Autocomplete
+                        multiple
+                        id="tags-filled"
+                        options={Tags.map((option) => option)}
+                        defaultValue={[Tags[0]]}
+                        freeSolo
+                        renderTags={(value, getTagProps) =>
+                            value.map((option, index) => (
+                                <Chip variant="outlined" label={option} {...getTagProps({ index })} />
+                            ))
+                        }
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                // variant="filled"
+                                label="Tags"
+                                placeholder="Your customize input..."
+                            />
+                        )}
+                    />
+                </div>
+                <div style={{ paddingTop: '20px' }}>
+                    <TextField
+                        InputProps={{
+                            inputProps: {
+                                style: { justifyContent: "right" },
+                            }
+                        }}
+                        id="outlined-multiline-static"
+                        label="Ingredients"
+                        multiline
+                        rows={4}
+                        fullWidth
+                    />
+                </div>
+                <div style={{ paddingTop: '20px' }}>
+                    <TextField
+                        InputProps={{
+
+                        }}
+                        id="outlined-multiline-static"
+                        label="Instruction"
+                        multiline
+                        rows={4}
+                        fullWidth
+                    />
+                </div>
+                <div style={{ paddingTop: '20px' }}>
+                    <Stack spacing={2} direction="row">
+                        <Button variant="outlined" >Upload</Button>
+                    </Stack>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/ViewRecipe.js
+++ src/pages/ViewRecipe.js
@@ -1,10 +1,87 @@
 import React from 'react';
 import './ViewRecipe.css';
+import Autocomplete from '@mui/material/Autocomplete';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import ReactStars from "react-rating-stars-component";
+// import { display } from '@mui/system';
 
 function ViewRecipe() {
-    return (  
+    const ratingChanged = (newRating) => {
+        console.log(newRating);
+    };
+    const Tags = ['Tatsy', 'Chicken', 'Pizza', 'Noodle', 'CleanEating', 'HealthyFood', 'JustEatRealFood', 'VeganFood', 'HealthyFoodRecipes', 'HealthyFoodLover', 'Popcorn']
+    return (
         <div className='ViewRecipe'>
-            Recipe viewing page
+            <div className='main-bodypart'>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <h3>Recipe Name: </h3>
+                    <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                        <TextField id="outlined-basic" label="Your input..." variant="outlined" />
+                    </Box>
+                </div>
+                <h2>By Anthony Trudy</h2>
+                <div style={{ paddingTop: '20px' }}>
+                    <Autocomplete
+                        multiple
+                        id="tags-readOnly"
+                        options={Tags.map((option) => option)}
+                        defaultValue={[Tags[0], Tags[1], Tags[2]]}
+                        readOnly
+                        renderInput={(params) => (
+                            <TextField {...params} label="Tags" placeholder="." />
+                        )}
+                    />
+                </div>
+                <div style={{ paddingTop: '20px' }}>
+                    <TextField
+                        InputProps={{
+                            inputProps: {
+                                style: { justifyContent: "right" },
+                            }
+                        }}
+                        id="outlined-multiline-static"
+                        label="Ingredients"
+                        multiline
+                        rows={4}
+                        fullWidth
+                    />
+                </div>
+                <div style={{ paddingTop: '20px' }}>
+                    <TextField
+                        InputProps={{
+
+                        }}
+                        id="outlined-multiline-static"
+                        label="Intruction"
+                        multiline
+                        rows={4}
+                        fullWidth
+                    />
+                </div>
+                <div style={{ display: 'flex', justifyContent: 'space-between', paddingTop: '20px' }}>
+                    <div style={{ display: 'flex' }}>
+                        <h3>Rate: </h3>
+                        <ReactStars
+                            count={5}
+                            onChange={ratingChanged}
+                            size={24}
+                            isHalf={true}
+                            emptyIcon={<i className="far fa-star"></i>}
+                            halfIcon={<i className="fa fa-star-half-alt"></i>}
+                            fullIcon={<i className="fa fa-star"></i>}
+                            activeColor="#ffd700"
+                        />
+                    </div>
+                    <div>
+                        <Stack spacing={2} direction="row">
+                            <Button variant="outlined" >Save</Button>
+                        </Stack>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
