398f05c1011e49b78b2d35a2a1f1f90ead14ec81
Long Le <lhlong1542002@gmail.com>
2023-01-05 00:35:57 +0700

    some fixes

--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App';
 import Login from './pages/Login';
-import {BrowserRouter as Router, HashRouter, Routes, Route} from "react-router-dom";
+import {HashRouter, Routes, Route} from "react-router-dom";
 import Register from './pages/Register';
-import ViewRecipe from './pages/ViewRecipe';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
     <Routes>
-      <Route path="*" element={<App/>} exact='True'>
-      </Route>
-      <Route path="Login" element={<Login/>} exact='True'>
-      </Route>
+      <Route path="*" element={<App/>} exact='True' />
+      <Route path="Login" element={<Login/>} exact='True' />
       <Route path="Register" element={<Register/>} exact='True' />
     </Routes>
   </HashRouter>
